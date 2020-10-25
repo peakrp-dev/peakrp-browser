b0aafe4e9aca6b40a0d8d284c691e5d3c6497c63
peakrp-dev <73438147+peakrp-dev@users.noreply.github.com>
2020-10-25 23:12:28 +0000

    update test wording

--- src/get-token.spec.js
+++ src/get-token.spec.js
@@ -1,24 +1,24 @@
 const getToken = require('./get-token');
 
-it('returns an empty string if the url is an emptry string', () => {
+test('should return an empty string if the url is an emptry string', () => {
   const url = '';
   const token = getToken(url);
   expect(token).toBe('');
 });
 
-it('returns an empty string if the url is not in the correct format', () => {
+test('should return an empty string if the url is not in the correct format', () => {
   const url = 'gibberish';
   const token = getToken(url);
   expect(token).toBe('');
 });
 
-it('returns the token when the url does not have a trailing slash', () => {
+test('should return the token when the url does not have a trailing slash', () => {
   const url = 'peakrp://token123';
   const token = getToken(url);
   expect(token).toBe('token123');
 });
 
-it('returns the token when the url has a trailing slash', () => {
+test('should return the token when the url has a trailing slash', () => {
   const url = 'peakrp://token123/';
   const token = getToken(url);
   expect(token).toBe('token123');
