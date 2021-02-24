2be43113641f25bba7690bf3be7c7daa308fe243
peakrp-dev <73438147+peakrp-dev@users.noreply.github.com>
2021-02-24 20:55:37 +0000

    Remove zip/rpm makers

--- forge.config.js
+++ forge.config.js
@@ -23,15 +23,7 @@ module.exports = {
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
       name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
       config: {},
     },
   ],
